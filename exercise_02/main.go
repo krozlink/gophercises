4dadaccba149b9c6445c3f5de4920b2a5f04861c
Daniel Taylor <dmtaylor17@gmail.com>
2018-08-28 16:46:02 +1000

    Started url shortener exercise

---
+++ exercise_02/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	urlshort "github.com/krozlink/gophercises/exercise_02/handler"
+	"net/http"
+)
+
+func main() {
+	mux := defaultMux()
+
+	pathToUrls := map[string]string{
+		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	}
+
+	mapHandler := urlshort.MapHandler(pathToUrls, mux)
+
+	yaml := `
+- path: /urlshort
+  url: https://github.com/gophercises/urlshort
+- path: /urlshort-final
+  url: https://github.com/gophercises/urlshort/tree/solution
+`
+	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+	if err != nil {
+		panic(err)
+	}
+
+	http.ListenAndServe(":8080", yamlHandler)
+}
+
+func defaultMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	return mux
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, world!")
+}
