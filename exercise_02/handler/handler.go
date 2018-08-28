cbea7f5f68c54689591458e8e65f2d99832be02d
Daniel Taylor <dmtaylor17@gmail.com>
2018-08-28 20:39:27 +1000

    Completed exercise 2

--- exercise_02/handler/handler.go
+++ exercise_02/handler/handler.go
@@ -1,39 +1,41 @@
 package urlshort
 
 import (
+	"fmt"
+	yaml "gopkg.in/yaml.v2"
 	"net/http"
 )
 
-// MapHandler will return an http.HandlerFunc (which also
-// implements http.Handler) that will attempt to map any
-// paths (keys in the map) to their corresponding URL (values
-// that each key in the map points to, in string format).
-// If the path is not provided in the map, then the fallback
-// http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
-
-	return fallback.ServeHTTP
-	//	TODO: Implement this...
-	// return nil
+	serve := func(w http.ResponseWriter, r *http.Request) {
+		if val, ok := pathsToUrls[r.RequestURI]; ok {
+			http.Redirect(w, r, val, 302)
+		} else {
+			fallback.ServeHTTP(w, r)
+		}
+	}
+	return serve
 }
 
-// YAMLHandler will parse the provided YAML and then return
-// an http.HandlerFunc (which also implements http.Handler)
-// that will attempt to map any paths to their corresponding
-// URL. If the path is not provided in the YAML, then the
-// fallback http.Handler will be called instead.
-//
-// YAML is expected to be in the format:
-//
-//     - path: /some-path
-//       url: https://www.some-url.com/demo
-//
-// The only errors that can be returned all related to having
-// invalid YAML data.
-//
-// See MapHandler to create a similar http.HandlerFunc via
-// a mapping of paths to urls.
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	// TODO: Implement this...
-	return fallback.ServeHTTP, nil
+	var paths []yamlPath
+	if err := yaml.Unmarshal(yml, &paths); err != nil {
+		return fallback.ServeHTTP, err
+	}
+
+	mapPaths := make(map[string]string)
+	for _, p := range paths {
+		mapPaths[p.Path] = p.URL
+	}
+
+	return MapHandler(mapPaths, fallback), nil
+}
+
+func serve(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.URL)
+}
+
+type yamlPath struct {
+	Path string `yaml:"path"`
+	URL  string `yaml:"url"`
 }
